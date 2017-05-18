bb3d58d3111984f0e6eafb630d13dc2c8a46ce18
Vaibhav Maheshwari <Vaibhav Maheshwari>
2017-05-18 22:12:51 +0530

    working on it

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log("server running");
+
+app.get('/',function(req,res) {
+	res.sendFile(__dirname + '/index.html')
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.sockets.on('connection',function(socket) {
+	connections.push(socket);
+	console.log("Connected: %s sockets connected", connections.length);
 
-  socket.on('disconnect', function(){
-    io.emit('disconnect');
-  });
+	//Disconnect 
+	socket.on('disconnect', function(data){
+	//if(!socket.username) return;
+	users.splice(users.indexOf(socket.username),1);
+	connections.splice(connections.indexOf(socket) , 1);
+	console.log("Disconnected: %s sockets connected", connections.length);
+	UpdateUserNames();
 
-});
+ 	});
+
+ 	socket.on('send message', function(data){
+ 		io.sockets.emit('new message' , { msg: data});
+ 	});
+ 	socket.emit('send message' ,"Hello World! ");
 
 
+ 	socket.on('new user', function(data,callback){
+ 		callback(true);
+ 		socket.username = data;
+ 		users.push(socket.username);
+ 		UpdateUserNames();
+ 	});
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+ 	function UpdateUserNames(){
+ 		io.sockets.emit('get users' , users);
+ 	}
 });
 
